2cf50307eca26fd30c37ccdf8d8b353aa6a2eb44
danieloselu3 <doselu3@gmail.com>
2024-06-11 13:40:49 +0300

    added example of a better coroutine

---
+++ asyncio/better_coroutine_example.py
@@ -0,0 +1,39 @@
+"""
+The aiohttp package is designed for 
+creating asynchronous HTTP clients and servers
+"""
+
+import aiohttp
+import asyncio
+import os
+import async_timeout
+
+async def download_coroutine(session, url):
+    with async_timeout.timeout(10):
+        async with session.get(url) as response:
+            filename = os.path.basename(url)
+            with open(filename, 'wb') as f_handle:
+                while True:
+                    chunk = await response.content.read(1024)
+                    if not chunk:
+                        break
+                    f_handle.write(chunk)
+            return await response.release()
+        
+
+async def main(loop):
+    urls =["https://www.irs.gov/pub/irs-pdf/f1040.pdf",
+           "https://www.irs.gov/pub/irs-pdf/f1040a.pdf",
+           "https://www.irs.gov/pub/irs-pdf/f1040ez.pdf",
+           "https://www.irs.gov/pub/irs-pdf/f1040es.pdf",
+           "https://www.irs.gov/pub/irs-pdf/f1040sb.pdf"]
+    
+    async with aiohttp.ClientSession(loop=loop) as session:
+        tasks = [download_coroutine(session, url) for url in urls]
+        await asyncio.gather(*tasks)
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(loop))
+
+
